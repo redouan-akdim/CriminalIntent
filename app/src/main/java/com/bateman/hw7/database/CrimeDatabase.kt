d6784a8c45fc33e064b6bd3a655ffe9707fe93b7
Redouan <redouan.akdim@web.de>
2024-05-09 04:05:43 +0200

    init commit

--- app/src/main/java/com/bateman/hw7/database/CrimeDatabase.kt
+++ app/src/main/java/com/bateman/hw7/database/CrimeDatabase.kt
@@ -7,7 +7,7 @@ import com.bateman.hw7.Crime
 import androidx.room.Database as Database1
 
 
-@Database1(entities = [Crime:: class], version=1)
+@Database1(entities = [Crime:: class], version=1, exportSchema = false)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase: RoomDatabase() {
     abstract fun crimeDAO(): CrimeDao
